f03bd3432f5ec9183b9c9320e68c96608ef5a9f7
Andres Gomez Urquiza <andres@fractal.ai>
2016-05-21 05:04:37 -0500

    tests

--- test/prime_table/core_test.clj
+++ test/prime_table/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [prime-table.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest ten-first-primes
+  (testing "Checking the quality of the prime generator"
+    (is (= [2 3 5 7 11 13 17 19 23 29] (primes 10)))))
+
+(deftest line-printer-quality
+  (testing "Checking the quality of the vector stringifyer"
+    (is (= "1\t2\t3" (line-string [1 2.0 3])))))
